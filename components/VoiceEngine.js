1e1440bf2f664b238a275b238d0c17ab671626ed
mogulflow <mogulflow@gmail.com>
2025-05-18 17:06:27 +0200

    Update VoiceEngine.js

--- components/VoiceEngine.js
+++ components/VoiceEngine.js
@@ -1,1 +1,11 @@
 // VoiceEngine.js component placeholder
+import React from 'react';
+
+export default function VoiceEngine() {
+  return (
+    <div>
+      <h2>Voice Engine</h2>
+      <p>Voice interaction system loading...</p>
+    </div>
+  );
+}
