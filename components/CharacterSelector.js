2c8b1420d41850e9b0a22eaa388fa0de05476c9b
mogulflow <mogulflow@gmail.com>
2025-05-18 17:05:16 +0200

    Update CharacterSelector.js

--- components/CharacterSelector.js
+++ components/CharacterSelector.js
@@ -1,1 +1,11 @@
 // CharacterSelector.js component placeholder
+import React from 'react';
+
+export default function CharacterSelector() {
+  return (
+    <div>
+      <h2>Character Selector</h2>
+      <p>This is where you'll choose your AI character.</p>
+    </div>
+  );
+}
