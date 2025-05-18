32af9f2218b3992e8cad99727bf9e6708ceb69aa
mogulflow <mogulflow@gmail.com>
2025-05-18 17:05:39 +0200

    Update ChatWidget.js

--- components/ChatWidget.js
+++ components/ChatWidget.js
@@ -1,1 +1,11 @@
 // ChatWidget.js component placeholder
+import React from 'react';
+
+export default function ChatWidget() {
+  return (
+    <div>
+      <h2>Chat Widget</h2>
+      <p>Chat functionality coming soon...</p>
+    </div>
+  );
+}
