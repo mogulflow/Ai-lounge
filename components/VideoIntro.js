60b2ce934598a52d170455dac6816aea61803e41
mogulflow <mogulflow@gmail.com>
2025-05-18 17:06:03 +0200

    Update VideoIntro.js

--- components/VideoIntro.js
+++ components/VideoIntro.js
@@ -1,1 +1,11 @@
 // VideoIntro.js component placeholder
+import React from 'react';
+
+export default function VideoIntro() {
+  return (
+    <div>
+      <h2>Video Intro</h2>
+      <p>Avatar introduction video will be here.</p>
+    </div>
+  );
+}
